d77c6bf4f6171007584040b9f8ef5efe12788fc8
rei <reiballa@gmail.com>
2022-05-20 13:28:58 +0200

    mongose

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 mongoose.connect( process.env.MONGODB_URI ||
-    "mongodb://localhost:27017/booksData",{ useNewUrlParser: true });
+    "mongodb://localhost:27017/booksData",{ useNewUrlParser: true, useUnifiedTopology:true });
 
 module.exports.books = require("./books.js");
